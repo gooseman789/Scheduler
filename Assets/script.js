5b2d389308b2e5301d4f4ece212d4f803d075b11
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2023-09-26 22:18:31 -0400

    finishing features

--- Assets/script.js
+++ Assets/script.js
@@ -28,15 +28,112 @@ $(document).ready(function () {
     localStorage.setItem(inputID, inputTEXT);}
   })
 
-  var currentTime = dayjs().format("HH")
+  var currentTime = dayjs().format("HH");
+  
+    $('div').children('textarea').removeClass('past');
+    $('div').children('textarea').addClass('future');
+if (currentTime === 9) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('present');
+  } else if (currentTime === 10) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('present');
+  } else if (currentTime === 11) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('past');
+    $('#hour-11').children('textarea').removeClass('future');
+    $('#hour-11').children('textarea').addClass('present');
+  } else if (currentTime === 12) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('past');
+    $('#hour-11').children('textarea').removeClass('future');
+    $('#hour-11').children('textarea').addClass('past');
+    $('#hour-12').children('textarea').removeClass('future');
+    $('#hour-12').children('textarea').addClass('present');
+  } else if (currentTime === 13) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('past');
+    $('#hour-11').children('textarea').removeClass('future');
+    $('#hour-11').children('textarea').addClass('past');
+    $('#hour-12').children('textarea').removeClass('future');
+    $('#hour-12').children('textarea').addClass('past');
+    $('#hour-13').children('textarea').removeClass('future');
+    $('#hour-13').children('textarea').addClass('present');
+  } else if (currentTime === 14) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('past');
+    $('#hour-11').children('textarea').removeClass('future');
+    $('#hour-11').children('textarea').addClass('present');
+    $('#hour-12').children('textarea').removeClass('future');
+    $('#hour-12').children('textarea').addClass('past');
+    $('#hour-13').children('textarea').removeClass('future');
+    $('#hour-13').children('textarea').addClass('past');
+    $('#hour-14').children('textarea').removeClass('future');
+    $('#hour-14').children('textarea').addClass('present');
+  } else if (currentTime === 15) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('past');
+    $('#hour-11').children('textarea').removeClass('future');
+    $('#hour-11').children('textarea').addClass('past');
+    $('#hour-12').children('textarea').removeClass('future');
+    $('#hour-12').children('textarea').addClass('past');
+    $('#hour-13').children('textarea').removeClass('future');
+    $('#hour-13').children('textarea').addClass('past');
+    $('#hour-14').children('textarea').removeClass('future');
+    $('#hour-14').children('textarea').addClass('past');
+    $('#hour-15').children('textarea').removeClass('future');
+    $('#hour-15').children('textarea').addClass('present');
+  } else if (currentTime === 16) {
+    $('#hour-9').children('textarea').removeClass('future');
+    $('#hour-9').children('textarea').addClass('past');
+    $('#hour-10').children('textarea').removeClass('future');
+    $('#hour-10').children('textarea').addClass('past');
+    $('#hour-11').children('textarea').removeClass('future');
+    $('#hour-11').children('textarea').addClass('past');
+    $('#hour-12').children('textarea').removeClass('future');
+    $('#hour-12').children('textarea').addClass('past');
+    $('#hour-13').children('textarea').removeClass('future');
+    $('#hour-13').children('textarea').addClass('past');
+    $('#hour-14').children('textarea').removeClass('future');
+    $('#hour-14').children('textarea').addClass('past');
+    $('#hour-15').children('textarea').removeClass('future');
+    $('#hour-15').children('textarea').addClass('past');
+    $('#hour-16').children('textarea').removeClass('future');
+    $('#hour-16').children('textarea').addClass('present');
+  } else {
+      $('#hour-9').children('textarea').removeClass('future');
+      $('#hour-9').children('textarea').addClass('past');
+      $('#hour-10').children('textarea').removeClass('future');
+      $('#hour-10').children('textarea').addClass('past');
+      $('#hour-11').children('textarea').removeClass('future');
+      $('#hour-11').children('textarea').addClass('past');
+      $('#hour-12').children('textarea').removeClass('future');
+      $('#hour-12').children('textarea').addClass('past');
+      $('#hour-13').children('textarea').removeClass('future');
+      $('#hour-13').children('textarea').addClass('past');
+      $('#hour-14').children('textarea').removeClass('future');
+      $('#hour-14').children('textarea').addClass('past');
+      $('#hour-15').children('textarea').removeClass('future');
+      $('#hour-15').children('textarea').addClass('past');
+      $('#hour-16').children('textarea').removeClass('future');
+      $('#hour-16').children('textarea').addClass('past');
+  }
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
 });
